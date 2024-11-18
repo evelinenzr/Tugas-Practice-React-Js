47dd4ee40568960891b5e3e9d2c8a2d43516f0b0
evelinenzr <evelinenazirah4@gmail.com>
2024-11-18 13:11:01 +0700

    Add files via upload

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,74 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'Blooming Beauty',
+      slug: 'Blooming Beauty',
+      name: "Blooming Beauty",
+      category: 'Bracelet',
+      price: 15_000,
+      stock: 20,
+      imageUrl: '/assets/images/bracelet 1.jpg',
+      description: `This Bracelet is the perfect blend of elegance and playfulness. Made from soft pink and green beads, and a charming white flower pendant, this bracelet will add a feminine and romantic touch to your look.`
+    },
+    {
+      id: 'Butterfly Kisses',
+      slug: 'Butterfly Kisses',
+      name: "Butterfly Kisses",
+      category: 'Bracelet',
+      price: 20_000,
+      stock: 10,
+      imageUrl: '/assets/images/bracelet 2.jpg',
+      description: `This Bracelet is the perfect blend of elegance and playfulness. Made from soft pink and green beads, and a charming white flower pendant, this bracelet will add a feminine and romantic touch to your look.`
+    },
+    {
+      id: 'Happy Keychains',
+      slug: 'Happy Keychains',
+      name: "Happy Keychains",
+      category: 'Keychain',
+      price: 8_000,
+      stock: 5,
+      imageUrl: `/assets/images/Keychain 2.jpg`,
+      description: `These adorable beaded keychains are the perfect way to add a touch of whimsy to your keys. Each keychain is handmade with high-quality materials and features a unique charm..`
+    },
+    {
+      id: 'Whimsical Charm',
+      slug: 'Whimsical Charm',
+      name: "Whimsical Charm",
+      category: 'Bracelet',
+      price: 8_000,
+      stock: 10,
+      imageUrl: `/assets/images/bracelet 3.jpg`,
+      description: `This Bracelet is the perfect blend of elegance and playfulness. Made from soft pink and green beads, and a charming white flower pendant, this bracelet will add a feminine and romantic touch to your look.`
+    },
+    {
+      id: 'Candy Hearts',
+      slug: 'Candy Hearts',
+      name: "Candy Hearts",
+      category: 'Keychain',
+      price: 20_000,
+      stock: 0,
+      imageUrl: `/assets/images/Keychain 1.jpg`,
+      description: `These adorable beaded keychains are the perfect way to add a touch of whimsy to your keys. Each keychain is handmade with high-quality materials and features a unique charm..`
+    },
+    {
+      id: 'Starry Night',
+      slug: 'Starry Night',
+      name: "Starry Night",
+      category: 'Bracelet',
+      price: 15_000,
+      stock: 0,
+      imageUrl: `/assets/images/bracelet 4.jpg`,
+      description: `This Bracelet is the perfect blend of elegance and playfulness. Made from soft pink and green beads, and a charming white flower pendant, this bracelet will add a feminine and romantic touch to your look.`
+    },
+    {
+      id: 'Sweet Melody ',
+      slug: 'Sweet Melody',
+      name: "Sweet Melody",
+      category: 'Keychain',
+      price: 25_000,
+      stock: 0,
+      imageUrl: `/assets/images/Keychain 3.jpg`,
+      description: `These adorable beaded keychains are the perfect way to add a touch of whimsy to your keys. Each keychain is handmade with high-quality materials and features a unique charm.`
+    },
+  ]
+}
